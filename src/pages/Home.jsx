0c62e899160be77377e83568e0e739dfaa7de447
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-11-14 21:17:05 +0530

    Added authmodal functionalities and styled home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../components/Nav";
+import AuthModal from "../components/AuthModal";
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
   const authToken = false;
   const handleClick = () => {
     console.log("Clicked");
+    setShowModal(true);
+    setIsSignUp(true);
   };
   return (
     <div className="overlay">
-      <Nav minimal={false} authToken={authToken} />
+      <Nav
+        minimal={false}
+        authToken={authToken}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
       <div className="home">
-        <h1>Swipe Right</h1>
+        <h1 className="primary-title">Swipe Right®</h1>
         <button className="primary-button" onClick={handleClick}>
           {authToken ? "Sign Out" : "Create Account"}
         </button>
+        {showModal && (
+          <AuthModal
+            setShowModal={setShowModal}
+            setIsSignUp={setIsSignUp}
+            isSignUp={isSignUp}
+          />
+        )}
       </div>
     </div>
   );

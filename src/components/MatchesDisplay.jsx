33ed8ac0e01e37b9ed7d63e30c07533513d0d993
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-11-16 01:21:42 +0530

    styled chat and dashboard

---
+++ src/components/MatchesDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MatchesDisplay = () => {
+  return <h2>Matched</h2>;
+};
+
+export default MatchesDisplay;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import ChatContainer from "../components/ChatContainer";
 const Dashboard = () => {
-  return <div>DashBoard</div>;
-};
+  const [characters, setCharacters] = useState([
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/MWAcQRM.jpeg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://i.imgur.com/MWAcQRM.jpeg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://i.imgur.com/MWAcQRM.jpeg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://i.imgur.com/MWAcQRM.jpeg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://i.imgur.com/MWAcQRM.jpeg",
+    },
+  ]);
+  const [lastDirection, setLastDirection] = useState();
 
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+  return (
+    <div className="dashboard">
+      <ChatContainer />
+      <div className="swiper-container">
+        <div className="card-container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+          <div className="swipe-info">
+            {lastDirection ? <p>You swiped {lastDirection}</p> : <p />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Dashboard;

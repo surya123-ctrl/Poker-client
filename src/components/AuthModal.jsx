7e81ccf3af9490c77a9db447bacddd710753f6e7
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-11-16 16:46:15 +0530

    set cookies for email, userId, token

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 const AuthModal = ({ setShowModal, setIsSignUp, isSignUp }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [cookie, setCookie, removeCookie] = useCookies(["user"]);
+  let navigate = useNavigate();
+
   console.log(email, password, confirmPassword);
   const handleClick = () => {
     setShowModal(false);
   };
   // const isSignUp = true;
-
-  const handleSubmit = (e) => {
+  // sds
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (isSignUp && password !== confirmPassword) {
         setError("Password need to match");
+        return;
       }
+      const response = await axios.post("http://localhost:8000/signup", {
+        email,
+        password,
+      });
+      setCookie("Email", response.data.email);
+      setCookie("userId", response.data.userId);
+      setCookie("AuthToken", response.data.token);
+      const success = response.status === 201;
+      if (success) navigate("/onboarding");
     } catch (error) {
       console.log(error);
     }
